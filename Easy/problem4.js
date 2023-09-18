b107c799cc242a879a8f902d280dbcba968997da
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-18 22:06:29 +0300

    problem4

---
+++ Easy/problem4.js
@@ -0,0 +1,8 @@
+// my solution
+function evenOrOdd(n) {
+  n % 2 === 0 ? "Even" : "Odd";
+}
+evenOrOdd(2); // output "Even"
+evenOrOdd(3); // output "Odd"
+evenOrOdd(-2); // output "Even"
+evenOrOdd(-3); // output "Odd"
