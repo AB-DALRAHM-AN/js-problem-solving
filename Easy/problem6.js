c24c5f17e06a515be1a95bc099a21f0f44829da8
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-19 00:45:50 +0300

    problem6

--- Easy/problem6.js
+++ Easy/problem6.js
@@ -1,5 +1,5 @@
 // Implement a function which convert the given boolean value into its string representation.
 // my solution
-function booleanToString(b){
+function booleanToString(b) {
   return b.toString();
 }
