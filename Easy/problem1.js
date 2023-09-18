e60927e165eb7d2c02e46b194844aa24f215e935
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-09-18 09:43:03 +0300

    the first problemSet

---
+++ Easy/problem1.js
@@ -0,0 +1,17 @@
+// my solution
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+}
+simpleMultiplication(4); // output 32
+simpleMultiplication(5); // output 45
+
+// best practice
+function simpleMultiplication(n) {
+  return n * (n % 2 ? 8 : 9);
+}; 
+simpleMultiplication(2); // output 16
+simpleMultiplication(1); // output 9
